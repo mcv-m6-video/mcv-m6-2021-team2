23181c06b070f4bac8e3c189f4c51ade6202ac0f
paudom <pdomingo@mobbio.com>
2021-03-09 20:25:19 +0100

    Task 3&4

--- week1.py
+++ week1.py
@@ -4,13 +4,15 @@ from pathlib import Path
 import cv2
 import numpy as np
 import copy
+import os
+import matplotlib.pyplot as plt
 
 from tools.annotation_reader import read_annotations
 from src.metrics import IoU, mAP
 from src.annotation import Annotation
-from src.frame import Frame
 from src.read_flow_img import read_flow_img
-from src.flow_metrics import calc_optical_flow
+from src.flow_metrics import calc_optical_flow, magnitude_flow
+from src.plot import plot_arrows
 
 def task11(dropout=None, generate_gt=None, noise=None):
     gt_path = Path.joinpath(Path(__file__).parent, "s03_c010-gt.txt")
@@ -68,14 +70,77 @@ def task12(det):
 
     return mAP(gt_annons, predict_annons)
 
-def task13():
+def task13_4():
     pred_000045_10 = read_flow_img(str(Path.joinpath(Path(__file__).parent, "pred_000045_10.png")))
     pred_000157_10 = read_flow_img(str(Path.joinpath(Path(__file__).parent, "pred_000157_10.png")))
     gt_000045_10 = read_flow_img(str(Path.joinpath(Path(__file__).parent, "gt_000045_10.png")))
     gt_000157_10 = read_flow_img(str(Path.joinpath(Path(__file__).parent, "gt_000157_10.png")))
 
-    print(calc_optical_flow(gt_000045_10, pred_000045_10))
+    # -- Frame 45 -- #
+    mse_45, error_45, msen_45, pepn_45 = calc_optical_flow(gt_000045_10, pred_000045_10)
+    print(msen_45, pepn_45)
 
-#task11()
-#print(task12('mask-rcnn'))
-task13()
+    # -- Histogram -- #
+    plt.figure()
+    plt.title('Error Histogram-45')
+    plt.hist(error_45, 25, color="orange")
+    plt.axvline(msen_45, color='g', linestyle='dashed', linewidth=1, label=f'MSEN {round(msen_45, 1)}')
+    plt.legend()
+    plt.savefig(os.path.join('results/week1/', 'histogram_45.png'))
+    plt.close()
+
+    # -- Mgnitude Flow -- #
+    magnitude_gt_45 = magnitude_flow(gt_000045_10)
+    plt.imshow(magnitude_gt_45, cmap='hot')
+    plt.axis('off')
+    plt.title('Magnitude GT Flow 45')
+    plt.savefig(os.path.join('results/week1', 'Magnitude GT Flow 45.png'))
+    plt.close()
+
+    magnitude_45_pred = magnitude_flow(pred_000045_10)
+    plt.imshow(magnitude_45_pred, cmap='hot')
+    plt.axis('off')
+    plt.title('Magnitude Pred Flow 45')
+    plt.savefig(os.path.join('results/week1', 'Magnitude Pred Flow 45.png'))
+    plt.close()
+
+    # -- Arrow T4-- #
+    img_45_gray = cv2.imread('000045_10_gray.png', cv2.IMREAD_GRAYSCALE)
+    plot_arrows(img_45_gray, gt_000045_10, 10, 45, 'GT')
+    plot_arrows(img_45_gray, pred_000045_10, 10, 45, 'PRED')
+
+    # -- Frame 157 -- #
+    mse_157, error_157, msen_157, pepn_157 = calc_optical_flow(gt_000157_10, pred_000157_10)
+    print(msen_157, pepn_157)
+
+    # -- Histogram -- #
+    plt.figure()
+    plt.title('Error Histogram-157')
+    plt.hist(error_157, 25, color="orange")
+    plt.axvline(msen_157, color='g', linestyle='dashed', linewidth=1, label=f'MSEN {round(msen_157, 1)}')
+    plt.legend()
+    plt.savefig(os.path.join('results/week1', 'histogram_157.png'))
+    plt.close()
+
+    # -- Mgnitude Flow -- #
+    magnitude_gt_157 = magnitude_flow(gt_000157_10)
+    plt.imshow(magnitude_gt_157, cmap='hot')
+    plt.axis('off')
+    plt.title('Magnitude GT Flow 157')
+    plt.savefig(os.path.join('results/week1', 'Magnitude GT Flow 157.png'))
+    plt.close()
+    
+    magnitude_157_pred = magnitude_flow(pred_000157_10)
+    plt.imshow(magnitude_157_pred, cmap='hot')
+    plt.axis('off')
+    plt.title('Magnitude Pred Flow 157')
+    plt.savefig(os.path.join('results/week1', 'Magnitude Pred Flow 157.png'))
+    plt.close()
+
+    # -- Arrow T4-- #
+    img_157_gray = cv2.imread('000157_10_gray.png', cv2.IMREAD_GRAYSCALE)
+    plot_arrows(img_157_gray, gt_000157_10, 10, 157, 'GT')
+    plot_arrows(img_157_gray, pred_000157_10, 10, 157, 'PRED')
+
+
+task13_4()
