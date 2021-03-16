ac519c1a9689c880c55a2a68adcd9b21c31693d6
drkztan <stanley.albayeros@gmail.com>
2021-03-17 00:45:54 +0100

    t3

--- week2_task3.py
+++ week2_task3.py
@@ -41,6 +41,7 @@ class BoundingBox():
         self.ybr = ybr
         self.confidence = score
         self.parked = parked
+        
 
     @property
     def bbox(self):
@@ -61,6 +62,7 @@ class BoundingBox():
     @property
     def center(self):
         return (int((self.xtl + self.xbr) / 2), int((self.ybr + self.ytl) / 2))
+        
     def get_bbox(self):
         return [self.xtl, self.ytl, self.xbr, self.ybr]
 
@@ -82,9 +84,9 @@ def bounding_boxes(mask, frame_id, min_height=100, max_height=600, min_width=120
 
 frame_files, frame_h, frame_w = get_frames_from_video(path=str(VIDEO_PATH), grayscale=True)
 
-mog_substractor  = cv2.bgsegm.createBackgroundSubtractorMOG()
-mog2_substractor = cv2.createBackgroundSubtractorMOG2()
-lsbp_substractor = cv2.bgsegm.createBackgroundSubtractorLSBP()
+mog_substractor  = cv2.bgsegm.createBackgroundSubtractorMOG(nmixtures=2, history=200)
+mog2_substractor = cv2.createBackgroundSubtractorMOG2(detectShadows=False, history=200)
+lsbp_substractor = cv2.bgsegm.createBackgroundSubtractorLSBP(minCount=5, mc=cv2.bgsegm.LSBP_CAMERA_MOTION_COMPENSATION_NONE)
 
 mog_bboxes_all_frames  = OrderedDict()
 mog2_bboxes_all_frames = OrderedDict()
@@ -94,6 +96,17 @@ FIRST_FRAME_ID = 535
 total_files =len(frame_files)
 print(f"Found {total_files} frames.")
 
+
+gt_bounding_boxes_orig = read_xml(str(FULL_ANNOTATION_PATH), include_parked=False)
+
+gt_bounding_boxes = list(filter(lambda x: x.frame >= FIRST_FRAME_ID, gt_bounding_boxes_orig))
+
+# gt_bounding_boxes = gt_bounding_boxes_orig[FIRST_FRAME_ID:]
+
+# print(len(gt_bounding_boxes_orig))
+print(len(gt_bounding_boxes))
+
+
 if (Path('preanotation.pickle').exists()):
     print("Found preanotation pkl, loading")
     with open('preanotation.pickle', 'rb') as f:
@@ -102,7 +115,25 @@ if (Path('preanotation.pickle').exists()):
 else:
     for i, frame_file in enumerate(frame_files[FIRST_FRAME_ID:], FIRST_FRAME_ID):
 
+
+
+
         frame = asarray(frame_file)
+
+        frame_bbox = frame.copy()
+        frame_bbox = cv2.cvtColor(frame_bbox,cv2.COLOR_GRAY2RGB)
+        frame_mog = frame_bbox.copy()
+        frame_mog2 = frame_bbox.copy()
+        frame_lsbp = frame_bbox.copy()
+
+        for item in gt_bounding_boxes:
+            if item.frame == i:
+                gtx1, gty1, gtx2, gty2 = item.get_bbox()
+                gtx1, gty1, gtx2, gty2 = int(gtx1), int(gty1), int(gtx2), int(gty2)
+                # print(f"{gtx1}, {gty1}, {gtx2}, {gty2}")
+                cv2.rectangle(frame_bbox, (gtx1, gty1), (gtx2, gty2), (255,255,0), 2)
+                
+
         mog_mask = mog_substractor.apply(frame)
         mog2_mask = mog2_substractor.apply(frame)
         lsbp_mask = lsbp_substractor.apply(frame)
@@ -120,16 +151,83 @@ else:
         mog2_bboxes_all_frames[i] = mog2_bboxes
         lsbp_bboxes_all_frames[i] = lsbp_bboxes
 
-        txt=f' working on frame {i} of {total_files}'
+        
+        # self.xtl = xtl
+        # self.ytl = ytl
+        # self.xbr = xbr
+        # self.ybr = ybr
+
+        font = cv2.FONT_HERSHEY_PLAIN
+        fontsize = 2
+
+        for item in mog_bboxes:
+            x1 = item.xtl
+            y1 = item.ytl
+            x2 = item.xbr
+            y2 = item.ybr
+            color = (255, 0, 255)
+            fontX = x2 - abs(x2-x1)
+            fontY = abs(y2-1)
+            thickness = 2
+
+            cv2.rectangle(frame_bbox, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame_bbox, ' MOG Box', (fontX, fontY),
+                        font, fontsize, color, thickness, cv2.LINE_AA)
+
+            cv2.rectangle(frame_mog, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame_mog, ' MOG Box', (fontX, fontY),
+                        font, fontsize, color, thickness, cv2.LINE_AA)
+
+        for item in mog2_bboxes:
+            x1 = item.xtl
+            y1 = item.ytl
+            x2 = item.xbr
+            y2 = item.ybr
+            color = (255, 125, 125)
+            fontX = x2 - abs(x2-x1)
+            fontY = abs(y2-1)
+
+            cv2.rectangle(frame_bbox, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame_bbox, ' MOG2 Box', (fontX, fontY),
+                        font, fontsize, color, thickness, cv2.LINE_AA)
+
+            cv2.rectangle(frame_mog2, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame_mog2, ' MOG2 Box', (fontX, fontY),
+                        font, fontsize, color, thickness, cv2.LINE_AA)
+
+        for item in lsbp_bboxes:
+            x1 = item.xtl
+            y1 = item.ytl
+            x2 = item.xbr
+            y2 = item.ybr
+            color = (125, 125, 255)
+            fontX = x2 - abs(x2-x1)
+            fontY = abs(y2-1)
+
+            cv2.rectangle(frame_bbox, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame_bbox, ' LSBP Box', (fontX, fontY),
+                        font, fontsize, color, thickness, cv2.LINE_AA)
+
+            cv2.rectangle(frame_lsbp, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame_lsbp, ' LSBP Box', (fontX, fontY),
+                        font, fontsize, color, thickness, cv2.LINE_AA)
+
+
+        cv2.imshow("window", frame_bbox)
+        cv2.waitKey(1)
+ 
+        cv2.imwrite(f"results/week2/all/frame_{i:07d}.jpg", frame_bbox)
+        cv2.imwrite(f"results/week2/mog/frame_{i:07d}.jpg", frame_mog)
+        cv2.imwrite(f"results/week2/mog2/frame_{i:07d}.jpg", frame_mog2)
+        cv2.imwrite(f"results/week2/lsbp/frame_{i:07d}.jpg", frame_lsbp)
+        # cv2.destroyAllWindows()  
+
+        txt=f' working on frame {i} of {total_files}. found {len(mog_bboxes)} bboxes'
 
         print_percent_done(i-FIRST_FRAME_ID, len(frame_files)-FIRST_FRAME_ID, title=txt)
 
     with open('preanotation.pickle', 'wb') as f:
             pickle.dump((mog_bboxes_all_frames, mog2_bboxes_all_frames, lsbp_bboxes_all_frames), f)
-
-gt_bounding_boxes = read_xml(str(FULL_ANNOTATION_PATH), include_parked=True)
-
-
 
 a = list(itertools.chain.from_iterable(mog_bboxes_all_frames.values()))
 b = list(itertools.chain.from_iterable(mog2_bboxes_all_frames.values()))
