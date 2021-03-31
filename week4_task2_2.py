e80536758d3b50d1c44c59739aa703ddc3869850
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-03-31 19:08:08 +0200

    week4 task2_2 working

--- week4_task2_2.py
+++ week4_task2_2.py
@@ -3,11 +3,12 @@ from vidstab import VidStab
 from pathlib import Path
 
 
-RESULTS_DIR = Path('Results/week4/task2_2')
+RESULTS_DIR = Path('results/week4/task2_2')
 
 
 def stabilize(input_video_path: str, output_video_path: str, kp_detector: str = 'GFTT', **kp_kwargs):
     # Off-the-shelf Stabilization
+    print(f' kp_detector {kp_detector} with args {kp_kwargs}')
     stabilizer = VidStab(kp_method=kp_detector, **kp_kwargs)
     stabilizer.stabilize(input_path=input_video_path, output_path=output_video_path)
 
@@ -15,19 +16,19 @@ def stabilize(input_video_path: str, output_video_path: str, kp_detector: str = 
 def task_2_2():
     INPUT_VIDEO_PATH = Path('week4_video.mov')
     params = [
-        ('GFTT', []),
-        ('BRISK', []),
-        ('DENSE', []),
-        ('FAST', []),
-        ('HARRIS', []),
-        ('MSER', []),
-        ('ORB', []),
-        ('STAR', []),
+        ('GFTT', {}),
+        ('BRISK', {}),
+        ('DENSE', {}),
+        ('FAST', {}),
+        ('HARRIS', {}),
+        ('MSER', {}),
+        ('ORB', {}),
+        ('STAR', {}),
     ]
 
     for kp_detector, kp_kwargs in params:
         output_video_path = RESULTS_DIR / f'output_{kp_detector}_{kp_kwargs}.avi'
-        stabilize(input_video_path=INPUT_VIDEO_PATH, output_video_path=output_video_path, kp_detector=kp_detector, **kp_kwargs)
+        stabilize(input_video_path=str(INPUT_VIDEO_PATH), output_video_path=str(output_video_path), kp_detector=kp_detector, **kp_kwargs)
 
 
 if __name__ == '__main__':
