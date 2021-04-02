794cca4cc5d96bec3b52a9920ee2299f5372a7bc
paudom <pdomingo@mobbio.com>
2021-04-02 20:44:55 +0200

    Task 2.1

--- week4_task1_2.py
+++ week4_task1_2.py
@@ -7,10 +7,10 @@ from src.pyflow import pyflow
 from src.hornschunk import HornSchunck
 from src.utils.flow_reader import read_flow_img
 from src.utils.plot import plot_optical_flow, plot_opt_flow_hsv, plot_img
-from src.metrics.optical_flow import evaluate_flow
+from src.metrics.optical_flow import evaluate_flow, compute_msen, compute_pepn
 
-RESULTS_DIR = Path('Results/week4')
-DATA_PATH = Path('data/optical_flow')
+RESULTS_DIR = Path('results/week4')
+DATA_PATH = Path('data/optical_flows')
 
 
 def task_1_2(algorithm='pyflow'):
@@ -24,37 +24,36 @@ def task_1_2(algorithm='pyflow'):
         im0 = np.atleast_3d(img_0.astype(float) / 255.)
         im1 = np.atleast_3d(img_1.astype(float) / 255.)
 
-        # flow options:
-        alpha = 0.012
-        ratio = 0.75
-        minWidth = 20
-        nOuterFPIterations = 7
-        nInnerFPIterations = 1
-        nSORIterations = 30
-        colType = 1  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
-
         tic = time.time()
         u, v, im2W = pyflow.coarse2fine_flow(
-            im0, im1, alpha, ratio, minWidth, nOuterFPIterations,
-            nInnerFPIterations, nSORIterations, colType
+            im0, im1, alpha=0.012, ratio=0.75, minWidth=20, nOuterFPIterations=7,
+            nInnerFPIterations=1, nSORIterations=30, colType=1
         )
         toc = time.time()
         flow = np.dstack((u, v))
     elif algorithm == 'hornschunck':
-        alpha = 0.012
-        iterations = 8
         tic = time.time()
-        u, v = HornSchunck(img_0, img_1, alpha=alpha, Niter=iterations)
+        u, v = HornSchunck(img_0, img_1, alpha=0.012, Niter=8)
         toc = time.time()
         flow = np.dstack((u, v))
+    elif algorithm == 'farneback':
+        tic = time.time()
+        flow = cv2.calcOpticalFlowFarneback(img_0, img_1, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        toc = time.time()
     else:
         raise ValueError(f'The Algorithm {algorithm} is not available')
 
     msen, pepn = evaluate_flow(flow_gt, flow)
     print(f'MSEN: {msen:.4f}, PEPN: {pepn:.4f}, runtime: {toc-tic:.3f}s')
+    print('Saving Results...')
+    hsv_gt = plot_opt_flow_hsv(flow_gt)
     hsv = plot_opt_flow_hsv(flow)
-    plot_img(hsv)
-    plot_optical_flow(img_0, flow, title=f'Optical Flow using {algorithm}', save_root=RESULTS_DIR)
+    plot_img(hsv, cmap=None, title=f'HSV_Representation_{algorithm}', save_root=RESULTS_DIR)
+    plot_img(hsv_gt, cmap=None, title=f'HSV_GT_Representation', save_root=RESULTS_DIR)
+    print('HSV saved')
+    plot_optical_flow(img_0, flow, title=f'Optical_Flow_using_{algorithm}', save_root=RESULTS_DIR)
+    plot_optical_flow(img_0, flow_gt, title=f'GT_Optical_Flow', save_root=RESULTS_DIR)
+    print('Done.')
 
 if __name__ == "__main__":
-    task_1_2(algorithm='pyflow')
+    task_1_2(algorithm='farneback')
---
+++ week4_task2_1.py
@@ -0,0 +1,64 @@
+import time
+import imageio
+import cv2
+import numpy as np
+
+from pathlib import Path
+from tqdm import trange
+from src.pyflow import pyflow
+
+RESULTS_DIR = Path('results/week4')
+WIDTH = 600
+HEIGHT = 350
+
+def task_2_1(algorithm='block_matching', method='average'):
+    cap = cv2.VideoCapture('data/test_stab.mp4')
+    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    out_video = imageio.get_writer(str(RESULTS_DIR / f'stabilization_{algorithm}_{method}.gif'), fps=fps)
+
+    previous_frame = None
+    for k in trange(0, n_frames, desc='Applying Stabilization'):
+        ok, frame = cap.read()
+        if not ok:
+            raise ValueError(f'The frame {k} could not be readed.')
+        current_frame = cv2.resize(frame, (WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+        if k == 0:
+            frame_stabilized = current_frame
+        else:
+            flow = compute_optical_flow(algorithm, previous_frame, current_frame)
+            frame_stabilized = apply_motion(current_frame, flow, method)
+        previous_frame = current_frame
+
+        out_video.append_data(frame_stabilized)
+
+
+def compute_optical_flow(algorithm, previous_frame, current_frame):
+    if algorithm == 'block_matching':
+        raise NotImplementedError('Here the code corresponding to computing the optical flow by block matcing should be implemented')
+    elif algorithm == 'pyflow':
+        u, v, im2W = pyflow.coarse2fine_flow(
+            previous_frame, current_frame, alpha=0.012, ratio=0.75, minWidth=20, nOuterFPIterations=7,
+            nInnerFPIterations=1, nSORIterations=30, colType=1
+        )
+        flow = np.dstack((u, v))
+    else:
+        raise ValueError('This option is not available.')
+    return flow
+
+
+def apply_motion(current_frame, flow, momentum, method):
+    if method == 'average':
+        x_t = flow[:, :, 0].mean()  # x translation
+        y_t = flow[:, :, 1].mean()  # y translation
+    elif method == 'median':
+        x_t = np.median(flow[:, :, 0])
+        y_t = np.median(flow[:, :, 1])
+    else:
+        raise ValueError('This option is not available')
+    H = np.array([
+        [1, 0, -x_t],
+        [0, 1, -y_t]
+    ], dtype=np.float32)
+    return cv2.warpAffine(current_frame, H, (WIDTH, HEIGHT))
