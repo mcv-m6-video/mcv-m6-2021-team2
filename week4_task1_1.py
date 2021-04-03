272153d957b2ac30e6f4f25f3de18b9009c73d67
ruben <ruben.bagan@gmail.com>
2021-04-03 18:58:10 +0200

    remove unused file

--- week4_task1_1.py
+++ week4_task1_1.py
@@ -4,7 +4,6 @@ import numpy as np
 from pathlib import Path
 from src.block_matching import block_matching
 from src.metrics.optical_flow import evaluate_flow
-from src.test_block import block_matching_flow
 
 def task1_1():
     previous_frame = cv2.imread(str(Path.joinpath(Path(__file__).parent, './data/000045_10.png')), cv2.IMREAD_GRAYSCALE)
@@ -12,10 +11,6 @@ def task1_1():
     flow_noc = cv2.imread(str(Path.joinpath(Path(__file__).parent, './data/gt_000045_10.png')))
 
     flow = block_matching(current_frame, previous_frame, True, 16, 32)
-
-    print(evaluate_flow(flow_noc, flow))
-
-    flow = block_matching_flow(previous_frame, current_frame, 16, 32)
 
     print(evaluate_flow(flow_noc, flow))
 
